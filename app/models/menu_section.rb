32c2b298166f4cd1c26c45680d081620e012830b
ziann <cheongziann@gmail.com>
2025-03-27 21:58:44 +0800

    initial set up

---
+++ app/models/menu_section.rb
@@ -0,0 +1,4 @@
+class MenuSection < ApplicationRecord
+  belongs_to :menu
+  belongs_to :section
+end
---
+++ app/models/section_item.rb
@@ -0,0 +1,4 @@
+class SectionItem < ApplicationRecord
+  belongs_to :section
+  belongs_to :item
+end
