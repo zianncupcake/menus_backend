70ea6cc590d441db6a114ea989eb3e8130801dbc
ziann <cheongziann@gmail.com>
2025-03-29 18:52:16 +0800

    feat: update menu mutation

---
+++ app/graphql/mutations/update_menu.rb
@@ -0,0 +1,25 @@
+module Mutations
+  class UpdateMenu < BaseMutation
+    argument :id, ID, required: true
+
+    argument :label, String, required: false
+    argument :state, String, required: false  
+    argument :start_date, GraphQL::Types::ISO8601Date, required: false
+    argument :end_date, GraphQL::Types::ISO8601Date, required: false
+
+
+    field :menu, Types::MenuType, null: true
+    field :errors, [String], null: false
+
+    def resolve(id:, **attributes)
+      menu = ::Menu.find_by(id: id)
+      return { menu: nil, errors: ["Menu not found"] } unless menu
+
+      if menu.update(attributes)
+        { menu: menu, errors: [] }
+      else
+        { menu: nil, errors: menu.errors.full_messages }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_menu, mutation: Mutations::CreateMenu
+    field :update_menu, mutation: Mutations::UpdateMenu
   end
 end
