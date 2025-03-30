769b57e4e0373baef90d801d3fbefe9ae1e571da
ziann <cheongziann@gmail.com>
2025-03-30 08:45:25 +0800

    chore: more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,8 @@ section2 = Section.create(
 )
 
 # Menu has many Sections via MenuSections
-MenuSection.create(menu: menu, section: section1, display_order: 2)
-MenuSection.create(menu: menu, section: section2, display_order: 1)
+MenuSection.create(menu: menu, section: section1, display_order: 1)
+MenuSection.create(menu: menu, section: section2, display_order: 2)
 
 # Create 3 Non-Configurable Items in Section 1
 item1 = Item.create(type: "Product", identifier: "plain_pizza", label: "Plain Pizza", description: "A simple pizza with cheese.", price: 9.00)
@@ -41,12 +41,14 @@ item3 = Item.create(type: "Product", identifier: "garlic_bread", label: "Garlic 
 
 # Create 2 Configurable Items in Section 2
 configurable_item1 = Item.create(type: "Product", identifier: "customizable_pizza", label: "Customizable Pizza", description: "Build your own pizza.", price: 10.00)
+configurable_item2 = Item.create(type: "Product", identifier: "customizable_salad", label: "Customizable Salad", description: "Build your own salad.", price: 20.00)
 
 # Section has many Items via SectionItems
 SectionItem.create(section: section1, item: item1, display_order: 3)
 SectionItem.create(section: section1, item: item2, display_order: 1)
 SectionItem.create(section: section1, item: item3, display_order: 2)
 SectionItem.create(section: section2, item: configurable_item1)
+SectionItem.create(section: section2, item: configurable_item2)
 
 # Create Modifier Groups for the Configurable Items (Pizza)
 topping_group = ModifierGroup.create(
@@ -84,5 +86,25 @@ Modifier.create(modifier_group: sauce_group, item: component6, price_override: 0
 # Item has many Modifier Groups via Item Modifier Groups (Pizza)
 ItemModifierGroup.create(item: configurable_item1, modifier_group: topping_group)
 ItemModifierGroup.create(item: configurable_item1, modifier_group: sauce_group)
+
+# Create Modifier Groups for the Configurable Items (Salad)
+dressing_group = ModifierGroup.create(
+  identifier: "choose_dressing",
+  label: "Choose Dressing",
+  selection_required_min: 1,
+  selection_required_max: 2
+)
+
+# Create Modifiers + Item (Component) for Toppings (Salad)
+component7 = Item.create(type: "Component", identifier: "balsamic", label: "Balsamic", description: "Balsamic for salad dressing", price: 1.00)
+component8 = Item.create(type: "Component", identifier: "ranch", label: "Ranch", description: "Ranch for salad dressing", price: 1.00)
+component9 = Item.create(type: "Component", identifier: "honey", label: "Honey", description: "Honey for salad dressing", price: 1.00)
+
+Modifier.create(modifier_group: dressing_group, item: component7, price_override: 0, display_order: 2)
+Modifier.create(modifier_group: dressing_group, item: component8, price_override: 0, display_order: 1)
+Modifier.create(modifier_group: dressing_group, item: component9, price_override: 0, display_order: 3)
+
+# Item has many Modifier Groups via Item Modifier Groups (Salad)
+ItemModifierGroup.create(item: configurable_item2, modifier_group: dressing_group)
 
 puts "Dummy data seeded successfully!"
